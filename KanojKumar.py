19c31e845d4e9f50503f0b5ecb4b5f2bf7790b21
Kanojkumar2001 <84522647+Kanojkumar2001@users.noreply.github.com>
2021-05-21 15:33:06 +0530

    Create KanojKumar.py

---
+++ KanojKumar.py
@@ -0,0 +1,21 @@
+name="CSE-A"
+print("Hello {} !How are you?".format(name))
+age=20
+print("Age is %d" %(age))
+print("Hello %s !Your Age is %d"%(name,age))
+per=7.48
+print("Hello %s !Your Age is %d and your grade is %.2f"%(name,age,per))
+a=60
+print("hexa %x"%(a))
+print("hexa %X"%(a))
+print("char %c"%(a))
+print("hexa %o"%(a))
+#output
+Hello CSE-A !How are you?
+Age is 20
+Hello CSE-A !Your Age is 20
+Hello CSE-A !Your Age is 20 and your grade is 7.48
+hexa 14
+hexa 14
+char B
+hexa 14
